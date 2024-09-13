5e8f28b830ccac26cb093bf9e60ed8a9b5f65047
Rajat sinha <rajatsinha5467@gmail.com>
2024-09-13 10:48:31 +0530

    ejs

--- mvc-01/routes/userRoute.js
+++ mvc-01/routes/userRoute.js
@@ -1,23 +1,27 @@
 const { Router } = require("express")
 const User = require("../model/user_schema")
-const { getUser, createUser, deleteUser, updateUser, upload } = require("../controllers/user.controller")
+const { getUser, createUser, deleteUser, updateUser, upload, getIndex } = require("../controllers/user.controller")
 
 const userRouter = Router()
 
 
 userRouter.get("/", getUser)
-userRouter.post("/",upload.single("img"), createUser)
+userRouter.post("/", upload.single("img"), createUser)
 userRouter.delete("/:id", deleteUser)
 userRouter.patch("/:id", updateUser)
 
 
-userRouter.post("/upload",upload.single("img"), (req, res) => {
+userRouter.post("/upload", upload.single("img"), (req, res) => {
     console.log("req.file", req.file);
 
     res.send("file uploaded successfully")
 })
 
-
+// index page
+userRouter.get("/login", (req, res) => {
+    res.render("login");
+})
+userRouter.get("/index", getIndex)
 
 
 module.exports = userRouter
